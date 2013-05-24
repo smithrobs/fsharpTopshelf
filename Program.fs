841e4ef541dc455c23c53793535080f7a2e60057
Robert Smith <smith.rob.s@gmail.com>
2013-05-24 13:39:05 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿namespace fsharpTopShelf
+
+module Program =
+    open Topshelf
+
+    open fsharpTopShelf.Runners
+
+    [<EntryPoint>]
+    let main argv = 
+        HostFactory.Run(fun host ->
+        
+            host.Service<TimerOutput>(Interop.init) |> ignore
+
+            host.RunAsLocalSystem() |> ignore
+
+            host.SetDescription "Example Fsharp Topshelf Host"
+            host.SetDisplayName "fsharptest"
+            host.SetServiceName "fsharptest"
+        ) |> ignore
+        0
---
+++ Runners.fs
@@ -0,0 +1,28 @@
+﻿namespace fsharpTopShelf
+
+module Runners =
+    open System
+    open System.Timers
+    open Topshelf
+
+    type TimerOutput(timer : Timer) =
+        new () = 
+            let timer = new Timer()
+            timer.Interval <- 1000.0
+            timer.AutoReset <- true
+            timer.Elapsed.Add(fun args ->
+                let timeNow = DateTime.Now.ToString()
+                printfn "It is %s and all is well" timeNow)
+            TimerOutput(timer)
+        member this.Start =
+            printfn "Start"
+            timer.Start()
+        member this.Stop =
+            printfn "Stop"
+            timer.Stop()
+
+    type Interop =
+        static member init(svc : ServiceConfigurators.ServiceConfigurator<TimerOutput>) =
+            svc.ConstructUsing(fun (name: string) -> new TimerOutput()) |> ignore
+            svc.WhenStarted(fun (tc : TimerOutput) -> tc.Start) |> ignore
+            svc.WhenStopped(fun (tc : TimerOutput) -> tc.Stop) |> ignore
